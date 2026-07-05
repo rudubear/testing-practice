955b1ab26e953b7d2d79db8795d2331139e081f6
RuduBear <134881527+rudubear@users.noreply.github.com>
2026-07-05 12:53:25 -0400

    initial set of files

---
+++ index.js
@@ -0,0 +1,24 @@
+function capitalize(myString) {
+  return myString;
+}
+
+function reverseString(myString) {
+  return myString;
+}
+
+const calculator = (function () {
+  return {
+    add: function () {},
+    subtract: function () {},
+    divide: function () {},
+    multiply: function () {},
+  };
+})();
+
+function ceaserCipher(myString) {
+  return myString;
+}
+
+function analyzeArray(myArray) {
+  return myArray;
+}
